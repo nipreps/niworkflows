8870f540f5d1655c34ab5f977a9637cfd33d6aec
Ross Blair <rblair2@stanford.edu>
2016-11-17 13:51:48 -0800

    moved nilearn requirement into info.py where it can be pulled in by setup.py

--- niworkflows/info.py
+++ niworkflows/info.py
@@ -49,7 +49,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.5',
 ]
 
-REQUIRES = ['nipype', 'future']
+REQUIRES = ['nipype', 'future', 'nilearn']
 SETUP_REQUIRES = []
 REQUIRES += SETUP_REQUIRES
 
