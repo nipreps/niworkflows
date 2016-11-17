470f1963bab2716ea723bef1ff95e271e03293e4
Ross Blair <rblair2@stanford.edu>
2016-11-17 13:45:58 -0800

    change import order

--- niworkflows/tests/test_niworkflows.py
+++ niworkflows/tests/test_niworkflows.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, division, print_function
-import os.path as op
+
 import numpy as np
-import pandas as pd
 import numpy.testing as npt
+import os.path as op
+import pandas as pd
+
 import niworkflows as sb
 
 data_path = op.join(sb.__path__[0], 'data')
