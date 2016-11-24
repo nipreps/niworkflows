a3b20d0c1fd53a4fdd9a3776cab01d9ba6ea878f
Shoshana Berleant <berleant@stanford.edu>
2016-11-24 07:27:15 +0000

    refactor test html validator

--- niworkflows/tests/test_validators.py
+++ niworkflows/tests/test_validators.py
@@ -1,31 +1,39 @@
 import unittest
 from niworkflows.common import report
 
-class TestHTMLValidator(unittest.TestCase):
+class TestValidator(unittest.TestCase):
+
+    unique_string = 'lala'
 
     def test_css_validator(self):
         pass
 
     def test_html_validator(self):
-        unique_string = 'lala'
-        valid_htmls = ['<super></super>', 'id=la' + unique_string,
-                       'id=' + unique_string + unique_string, 'id = a' + unique_string, '',
-                       '<div class=heya id =yall' + unique_string, unique_string,
-                       'ID=what' + unique_string, 'id=id' + unique_string, 'Id=a' + unique_string,
-                       'iD=a' + unique_string]
-        invalid_htmls = ['<body>', '<head id=' + unique_string + 'x', '<header', '<footer',
-                         '<mainlandchina', 'id = ' + unique_string, 'id=wassup',
-                         'id=s ' + unique_string, 'id=' + unique_string + ' s',
-                         'id=' + unique_string[1:], '  ID =x ' + unique_string + 'x',
-                         '<p id=a' + unique_string + '></p><p id=a' + unique_string + '>']
-
-        validator = report.HTMLValidator(unique_string=unique_string)
-            
-        for html in valid_htmls:
+        def _use_validator(validator, html):
             validator.feed(html)
             validator.close()
-        
-        for html in invalid_htmls:
-            validator.feed(html)
+
+        self._tester(
+            valid_strings=['<super></super>', 'id=la' + self.unique_string,
+                           'id=' + self.unique_string + self.unique_string,
+                           'id = a' + self.unique_string, '',
+                           '<div class=heya id =yall' + self.unique_string, self.unique_string,
+                           'ID=what' + self.unique_string, 'id=id' + self.unique_string,
+                           'Id=a' + self.unique_string, 'iD=a' + self.unique_string],
+            invalid_strings=['<body>', '<head id=' + self.unique_string + 'x', '<header',
+                             '<footer', '<mainlandchina', 'id = ' + self.unique_string,
+                             'id=wassup', 'id=s ' + self.unique_string,
+                             'id=' + self.unique_string + ' s', 'id=' + self.unique_string[1:],
+                             '  ID =x ' + self.unique_string + 'x',
+                             '<p id=a' + self.unique_string + '></p><p id=a' +
+                             self.unique_string + '>'],
+            validator = report.HTMLValidator(unique_string=self.unique_string),
+            func=_use_validator)
+
+    def _tester(self, validator, valid_strings, invalid_strings, func):
+        for string in valid_strings:
+            func(validator, string)
+
+        for string in invalid_strings:
             with self.assertRaises(ValueError):
-                validator.close()
+                func(validator, string)
