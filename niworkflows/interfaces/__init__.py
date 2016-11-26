4896ed55aaa5c490c69e29abb8b9616fc7b80804
Oscar Esteban <code@oscaresteban.es>
2016-11-25 17:46:57 -0800

    import bet from masks

--- niworkflows/interfaces/__init__.py
+++ niworkflows/interfaces/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-from .segmentation import (FASTRPT as FAST,
-                           BETRPT as BET)
+from .masks import BETRPT as BET
+from .segmentation import (FASTRPT as FAST)
 from .registration import (FLIRTRPT as FLIRT,
                            ApplyXFMRPT as FSLApplyXFM,
                            ANTSRegistrationRPT as ANTSRegistration)
