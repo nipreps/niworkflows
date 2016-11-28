c3b863468725055a777c7d400cbe059ffd6ac1f8
oesteban <code@oscaresteban.es>
2016-11-28 11:00:39 -0800

    fix mni registration leftovers, fix imports

--- niworkflows/interfaces/__init__.py
+++ niworkflows/interfaces/__init__.py
@@ -4,5 +4,4 @@ from __future__ import absolute_import, division, print_function
 from .segmentation import (FASTRPT as FAST,
                            BETRPT as BET)
 from .registration import (FLIRTRPT as FLIRT,
-                           ApplyXFMRPT as FSLApplyXFM,
-                           ANTSRegistrationRPT as ANTSRegistration)
+                           RobustMNINormalizationRPT as RobustMNINormalization)
