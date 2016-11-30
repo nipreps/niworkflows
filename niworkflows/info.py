10f87614776e9aaa763a25b1782af7fecf05796a
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-11-29 20:56:46 -0800

    specify version, add nose dep

--- niworkflows/info.py
+++ niworkflows/info.py
@@ -49,13 +49,13 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.5',
 ]
 
-REQUIRES = ['nipype==dev', 'future', 'nilearn', 'sklearn', 'pandas', 'matplotlib',
+REQUIRES = ['nipype==8ddc', 'future', 'nilearn', 'sklearn', 'pandas', 'matplotlib',
             'jinja2', 'svgutils', 'tinycss']
 SETUP_REQUIRES = []
 REQUIRES += SETUP_REQUIRES
 
-LINKS_REQUIRES = ['git+https://github.com/nipy/nipype.git@8ddca5a03fcad26887c862dc23c82ef23f2ee506#egg=nipype-dev',]
-TESTS_REQUIRES = ['mock', 'codecov', 'pytest-xdist']
+LINKS_REQUIRES = ['git+https://github.com/nipy/nipype.git@8ddca5a03fcad26887c862dc23c82ef23f2ee506#egg=nipype-8ddc',]
+TESTS_REQUIRES = ['mock', 'codecov', 'pytest-xdist', 'nose']
 EXTRA_REQUIRES = {
     'doc': ['sphinx'],
     'tests': TESTS_REQUIRES,
