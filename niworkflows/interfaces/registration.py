6fd8492f36dd27df17614d2ee15aaa39377b1175
oesteban <code@oscaresteban.es>
2016-12-01 16:51:35 -0800

    [ENH] Adds the report capable interface of ants.Registration
    Close #54

--- niworkflows/interfaces/registration.py
+++ niworkflows/interfaces/registration.py
@@ -5,7 +5,7 @@ ReportCapableInterfaces for registration tools
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from nipype.interfaces import fsl
+from nipype.interfaces import fsl, ants
 from niworkflows.anat import mni
 import niworkflows.common.report as nrc
 from niworkflows import NIWORKFLOWS_LOG
@@ -31,6 +31,25 @@ class RobustMNINormalizationRPT(
                              self._fixed_image, self._moving_image)
 
 
+class ANTSRegistrationInputSpecRPT(nrc.RegistrationRCInputSpec,
+                                   ants.registration.RegistrationInputSpec):
+    pass
+
+class ANTSRegistrationOutputSpecRPT(nrc.ReportCapableOutputSpec,
+                                    ants.registration.RegistrationOutputSpec):
+    pass
+
+class ANTSRegistrationRPT(nrc.RegistrationRC, ants.Registration):
+    input_spec = ANTSRegistrationInputSpecRPT
+    output_spec = ANTSRegistrationOutputSpecRPT
+
+    def _post_run_hook(self, runtime):
+        self._fixed_image = self.inputs.fixed_image
+        self._moving_image = self.aggregate_outputs().warped_image
+        NIWORKFLOWS_LOG.info('Report - setting fixed (%s) and moving (%s) images',
+                             self._fixed_image, self._moving_image)
+
+
 class FLIRTInputSpecRPT(nrc.RegistrationRCInputSpec,
                         fsl.preprocess.FLIRTInputSpec):
     pass
@@ -49,23 +68,13 @@ class FLIRTRPT(nrc.RegistrationRC, fsl.FLIRT):
         NIWORKFLOWS_LOG.info('Report - setting fixed (%s) and moving (%s) images',
                              self._fixed_image, self._moving_image)
 
-class ApplyXFMInputSpecRPT(nrc.RegistrationRCInputSpec,
-                           fsl.preprocess.ApplyXFMInputSpec):
-    pass
 
 
+# COMMENTED OUT UNTIL WE REALLY IMPLEMENT IT
+# class ApplyXFMInputSpecRPT(nrc.RegistrationRCInputSpec,
+#                            fsl.preprocess.ApplyXFMInputSpec):
+#     pass
+#
 # class ApplyXFMRPT(FLIRTRPT):
 #     ''' ApplyXFM is a wrapper around FLIRT. ApplyXFMRPT is a wrapper around FLIRTRPT.'''
 #     input_spec = ApplyXFMInputSpecRPT
-
-# class ANTSRegistrationInputSpecRPT(nrc.RegistrationRCInputSpec,
-#                                    ants.registration.RegistrationInputSpec):
-#     pass
-
-# class ANTSRegistrationOutputSpecRPT(nrc.ReportCapableOutputSpec,
-#                                     ants.registration.RegistrationOutputSpec):
-#     pass
-
-# class ANTSRegistrationRPT(nrc.RegistrationRC, ants.Registration):
-#     input_spec = ANTSRegistrationInputSpecRPT
-#     output_spec = ANTSRegistrationOutputSpecRPT
