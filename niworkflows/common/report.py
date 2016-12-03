7d0b10245bfc00a280dee17615040edfa8d9c237
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-12-03 09:06:11 -0800

    use proper masks

--- niworkflows/common/report.py
+++ niworkflows/common/report.py
@@ -133,12 +133,12 @@ class RegistrationRC(ReportCapableInterface):
         if self._fixed_image_mask:
             fixed_image_nii = unmask(apply_mask(fixed_image_nii,
                                                 self._fixed_image_mask),
-                                     fixed_image_nii)
+                                     self._fixed_image_mask)
 
         if self._moving_image_mask:
             moving_image_nii = unmask(apply_mask(moving_image_nii,
                                                  self._moving_image_mask),
-                                      moving_image_nii)
+                                      self._moving_image_mask)
 
 
         # Call composer
