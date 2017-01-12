afaed2191eccf9d6331542527c18ec05c3e48d30
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2017-01-12 14:54:00 -0800

    update version

--- niworkflows/info.py
+++ niworkflows/info.py
@@ -11,12 +11,12 @@ as well as for open-source software distribution.
 from __future__ import absolute_import, division, print_function
 import datetime
 
-__version__ = '0.0.4a0'
+__version__ = 'dev'
 __packagename__ = 'niworkflows'
 __author__ = 'The CRN developers'
 __copyright__ = 'Copyright {}, Center for Reproducible Neuroscience, Stanford University'.format(
     datetime.datetime.now().year)
-__credits__ = ['Oscar Esteban', 'Ross Blair', 'Shoshana L. Berleant', 'Chris F. Gorgolewski',
+__credits__ = ['Oscar Esteban', 'Ross Blair', 'Shoshana L. Berleant', 'Chris Gorgolewski',
                'Russell A. Poldrack']
 __license__ = '3-clause BSD'
 __maintainer__ = 'Oscar Esteban'
