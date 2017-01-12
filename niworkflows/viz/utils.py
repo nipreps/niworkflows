165dcf602c5ac5e15e933a07cfdc0a58b61bfe7c
Ross Blair <rosswilsonblair@gmail.com>
2017-01-12 13:50:41 -0800

    blank the values of height and width in the svg etree object

--- niworkflows/viz/utils.py
+++ niworkflows/viz/utils.py
@@ -280,6 +280,7 @@ def compose_view(bg_svgs, fg_svgs, ref=0, out_file='report.svg'):
     ]
     fig.append(newroots)
     out_file = op.abspath(out_file)
+    fig.root.setsize("", "")
     fig.save(out_file)
 
     # Add styles for the flicker animation
