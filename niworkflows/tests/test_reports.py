392dab23a99fb7f9ed2dbc67281acb0fe84c0024
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2017-01-19 18:24:18 -0800

    added a test

--- niworkflows/tests/test_reports.py
+++ niworkflows/tests/test_reports.py
@@ -160,3 +160,22 @@ class TestFASTRPT(unittest.TestCase):
                                    probability_maps=True, out_basename='test')
 
         _smoke_test_report(report_interface, 'testFAST_no_segments.html')
+
+
+class TestCompression(unittest.TestCase):
+
+    def test_compression(self):
+        ''' test if compression makes files smaller '''
+        uncompressed_int = BETRPT(in_file=MNI_2MM, generate_report=True,
+                                  mask=True, compress_report=False)
+        uncompressed_int.run()
+        uncompressed_report = uncompressed_int.inputs.out_report
+
+        compressed_int = BETRPT(in_file=MNI_2MM, generate_report=True,
+                                mask=True, compress_report=True)
+        compressed_int.run()
+        compressed_report = compressed_int.inputs.out_report
+
+        unittest.TestCase.assertTrue(int(os.stat(uncompressed_report).st_size) > int(os.stat(compressed_report).st_size),
+                                        'An uncompressed report is bigger than '
+                                        'a compressed report')
