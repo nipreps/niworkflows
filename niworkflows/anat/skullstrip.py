0c3183b266daa4a3e485cb286fb290e4352c8e8f
oesteban <code@oscaresteban.es>
2017-05-18 15:45:18 -0700

    fix header after N4

--- niworkflows/anat/skullstrip.py
+++ niworkflows/anat/skullstrip.py
@@ -5,6 +5,7 @@ from nipype.interfaces import afni
 from nipype.interfaces import fsl
 from nipype.interfaces import utility as niu
 from nipype.pipeline import engine as pe
+from niworkflows.interfaces.utils import CopyHeader
 
 def afni_wf(name='AFNISkullStripWorkflow', n4_nthreads=1):
     """
@@ -23,9 +24,11 @@ quality-assessment-protocol/blob/master/qap/anatomical_preproc.py#L105
     outputnode = pe.Node(niu.IdentityInterface(
         fields=['bias_corrected', 'out_file', 'out_mask', 'bias_image']), name='outputnode')
 
-    inu_n4 = pe.Node(ants.N4BiasFieldCorrection(
-        dimension=3, save_bias=True, num_threads=n4_nthreads), num_threads=n4_nthreads,
-                     name='CorrectINU')
+    inu_n4 = pe.Node(
+        ants.N4BiasFieldCorrection(dimension=3, save_bias=True, num_threads=n4_nthreads),
+        n_procs=n4_nthreads,
+        name='inu_n4')
+    orig_hdr = pe.Node(CopyHeader(), name='orig_hdr')
 
     sstrip = pe.Node(afni.SkullStrip(outputtype='NIFTI_GZ'), name='skullstrip')
     sstrip_orig_vol = pe.Node(afni.Calc(
@@ -34,14 +37,16 @@ quality-assessment-protocol/blob/master/qap/anatomical_preproc.py#L105
 
     workflow.connect([
         (inputnode, sstrip_orig_vol, [('in_file', 'in_file_a')]),
+        (inputnode, orig_hdr, [('in_file', 'hdr_file')]),
         (inputnode, inu_n4, [('in_file', 'input_image')]),
-        (inu_n4, sstrip, [('output_image', 'in_file')]),
+        (inu_n4, orig_hdr, [('output_image', 'in_file')]),
+        (orig_hdr, sstrip, [('out_file', 'in_file')]),
         (sstrip, sstrip_orig_vol, [('out_file', 'in_file_b')]),
         (sstrip_orig_vol, binarize, [('out_file', 'in_file')]),
         (sstrip_orig_vol, outputnode, [('out_file', 'out_file')]),
+        (orig_hdr, outputnode, [('out_file', 'bias_corrected')]),
         (binarize, outputnode, [('out_file', 'out_mask')]),
-        (inu_n4, outputnode, [('output_image', 'bias_corrected'),
-                              ('bias_image', 'bias_image')])
+        (inu_n4, outputnode, [('bias_image', 'bias_image')])
     ])
     return workflow
 
---
+++ niworkflows/interfaces/utils.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Utilities
+
+"""
+
+import os.path as op
+import nibabel as nb
+from nipype.interface.base import File, OutputMultiPath, BaseInterfaceInputSpec, TraitedSpec
+
+from .base import SimpleInterface
+
+class CopyHeaderInputSpec(BaseInterfaceInputSpec):
+    in_file = File(exists=True, mandatory=True, desc='the file we get the data from')
+    hdr_file = File(exists=True, mandatory=True, desc='the file we get the header from')
+
+class CopyHeaderOutputSpec(TraitedSpec):
+    out_file = OutputMultiPath(File(exists=True, desc='written file path'))
+
+class CopyHeader(SimpleInterface):
+    input_spec = CopyHeaderInputSpec
+    output_spec = CopyHeaderOutputSpec
+
+    def _run_interface(self, runtime):
+
+        hdr = nb.load(self.inputs.hdr_file).get_header().copy()
+        aff = nb.load(self.inputs.hdr_file).get_affine()
+        data = nb.load(self.inputs.in_file).get_data()
+
+        fname, ext = op.splitext(op.basename(self.inputs.in_file))
+        if ext == '.gz':
+            fname, ext2 = op.splitext(fname)
+            ext = ext2 + ext
+
+        out_name = op.abspath('{}_fixhdr{}'.format(fname, ext))
+        nb.Nifti1Image(data, aff, hdr).to_filename(out_name)
+        self._results['out_file'] = out_name
+        return runtime
