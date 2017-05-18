eed0ea8731225511c0d310016debe9740884ae87
oesteban <code@oscaresteban.es>
2017-05-18 10:44:27 -0700

    [ENH] Add 3dUnifize to skull-stripping workflow
    Comes from: https://github.com/poldracklab/mriqc/issues/489

--- niworkflows/anat/skullstrip.py
+++ niworkflows/anat/skullstrip.py
@@ -6,13 +6,16 @@ from nipype.interfaces import fsl
 from nipype.interfaces import utility as niu
 from nipype.pipeline import engine as pe
 
-def afni_wf(name='AFNISkullStripWorkflow'):
+def afni_wf(name='AFNISkullStripWorkflow', unifize=True):
     """
     Skull-stripping workflow
 
-    Derived from the codebase of the QAP:
-    https://github.com/preprocessed-connectomes-project/\
-quality-assessment-protocol/blob/master/qap/anatomical_preproc.py#L105
+    Originally derived from the `codebase of the
+    QAP <https://github.com/preprocessed-connectomes-project/\
+quality-assessment-protocol/blob/master/qap/anatomical_preproc.py#L105>`_.
+    Now, this workflow includes :abbr:`INU (intensity non-uniformity)` correction
+    using the N4 algorithm and (optionally) intensity harmonization using
+    ANFI's ``3dUnifize``.
 
 
     """
@@ -25,22 +28,38 @@ quality-assessment-protocol/blob/master/qap/anatomical_preproc.py#L105
 
     inu_n4 = pe.Node(ants.N4BiasFieldCorrection(dimension=3, save_bias=True),
                      name='CorrectINU')
-
     sstrip = pe.Node(afni.SkullStrip(outputtype='NIFTI_GZ'), name='skullstrip')
     sstrip_orig_vol = pe.Node(afni.Calc(
         expr='a*step(b)', outputtype='NIFTI_GZ'), name='sstrip_orig_vol')
     binarize = pe.Node(fsl.Threshold(args='-bin', thresh=1.e-3), name='binarize')
 
+    if unifize:
+        # Add two unifize steps, pre- and post- skullstripping.
+        inu_uni_0 = pe.Node(afni.Unifize(), name='Unifize_pre_skullstrip')
+        inu_uni_1 = pe.Node(afni.Unifize(gm=True), name='Unifize_post_skullstrip')
+        workflow.connect([
+            (inu_n4, inu_uni_0, [('output_image', 'in_file')]),
+            (inu_uni_0, sstrip, [('out_file', 'in_file')]),
+            (sstrip_orig_vol, inu_uni_1, [('out_file', 'in_file')]),
+            (inu_uni_1, outputnode, [('out_file', 'out_file')]),
+            (inu_uni_0, outputnode, [('out_file', 'bias_corrected')]),
+        ])
+    else:
+        workflow.connect([
+            (inu_n4, sstrip, [('output_image', 'in_file')]),
+            (sstrip, sstrip_orig_vol, [('out_file', 'in_file_b')]),
+            (sstrip_orig_vol, outputnode, [('out_file', 'out_file')]),
+            (inu_n4, outputnode, [('output_image', 'bias_corrected')]),
+        ])
+
+
+    # Remaining connections
     workflow.connect([
         (inputnode, sstrip_orig_vol, [('in_file', 'in_file_a')]),
         (inputnode, inu_n4, [('in_file', 'input_image')]),
-        (inu_n4, sstrip, [('output_image', 'in_file')]),
-        (sstrip, sstrip_orig_vol, [('out_file', 'in_file_b')]),
         (sstrip_orig_vol, binarize, [('out_file', 'in_file')]),
-        (sstrip_orig_vol, outputnode, [('out_file', 'out_file')]),
         (binarize, outputnode, [('out_file', 'out_mask')]),
-        (inu_n4, outputnode, [('output_image', 'bias_corrected'),
-                              ('bias_image', 'bias_image')])
+        (inu_n4, outputnode, [('bias_image', 'bias_image')]),
     ])
     return workflow
 
