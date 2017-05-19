9ac3d303ecff8f65af014c458577deef0b4a87b8
oesteban <code@oscaresteban.es>
2017-05-18 17:17:16 -0700

    various fixes

--- niworkflows/interfaces/utils.py
+++ niworkflows/interfaces/utils.py
@@ -7,10 +7,9 @@ Utilities
 
 """
 
-import os.path as op
 import nibabel as nb
+from nipype.utils.filemanip import fname_presuffix
 from nipype.interfaces.base import File, OutputMultiPath, BaseInterfaceInputSpec, TraitedSpec
-
 from .base import SimpleInterface
 
 class CopyHeaderInputSpec(BaseInterfaceInputSpec):
@@ -25,17 +24,13 @@ class CopyHeader(SimpleInterface):
     output_spec = CopyHeaderOutputSpec
 
     def _run_interface(self, runtime):
+        in_img = nb.load(self.inputs.hdr_file)
+        out_img = nb.load(self.inputs.in_file, mmap=False)
+        new_img = out_img.__class__(out_img.get_data(), in_img.affine, in_img.header)
+        new_img.set_data_dtype(out_img.get_data_dtype())
 
-        hdr = nb.load(self.inputs.hdr_file).get_header().copy()
-        aff = nb.load(self.inputs.hdr_file).get_affine()
-        data = nb.load(self.inputs.in_file).get_data()
-
-        fname, ext = op.splitext(op.basename(self.inputs.in_file))
-        if ext == '.gz':
-            fname, ext2 = op.splitext(fname)
-            ext = ext2 + ext
-
-        out_name = op.abspath('{}_fixhdr{}'.format(fname, ext))
-        nb.Nifti1Image(data, aff, hdr).to_filename(out_name)
+        out_name = fname_presuffix(self.inputs.in_file,
+                                   suffix='_fixhdr', newpath='.')
+        new_img.to_filename(out_name)
         self._results['out_file'] = out_name
         return runtime
