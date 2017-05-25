05c803dc2fe1d83c932938bb8477d00860c23625
Christopher J. Markiewicz <markiewicz@stanford.edu>
2017-05-25 13:06:25 -0400

    Update dev version

--- niworkflows/info.py
+++ niworkflows/info.py
@@ -11,7 +11,7 @@ as well as for open-source software distribution.
 from __future__ import absolute_import, division, print_function
 import datetime
 
-__version__ = '0.0.7-dev'
+__version__ = '0.0.8-dev'
 __packagename__ = 'niworkflows'
 __author__ = 'The CRN developers'
 __copyright__ = 'Copyright {}, Center for Reproducible Neuroscience, Stanford University'.format(
