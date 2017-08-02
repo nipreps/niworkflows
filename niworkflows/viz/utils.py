3460cce20547596776ed75059da1d5e5721f4455
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2017-08-02 16:50:19 -0700

    better handle rare situations

--- niworkflows/viz/utils.py
+++ niworkflows/viz/utils.py
@@ -504,8 +504,17 @@ def plot_melodic_components(melodic_dir, in_file, tr=None,
     if not tr:
         tr = in_nii.header.get_zooms()[3]
         units = in_nii.header.get_xyzt_units()
-        if units and units[-1] == 'msec':
-            tr = tr/1000.0
+        if units:
+            if units[-1] == 'msec':
+                tr = tr / 1000.0
+            elif units[-1] == 'usec':
+                tr = tr / 1000000.0
+            elif units[-1] != 'sec':
+                NIWORKFLOWS_LOG.warn('Unknown repetition time units '
+                                     'specified - assuming seconds')
+        else:
+            NIWORKFLOWS_LOG.warn(
+                'Repetition time units not specified - assuming seconds')
 
     from nilearn.input_data import NiftiMasker
     from nilearn.plotting import plot_roi
