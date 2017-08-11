bcfeaa53cbb0c4190523e33027017f0ee444de4d
Christopher J. Markiewicz <markiewicz@stanford.edu>
2017-08-11 14:52:46 -0400

    Update dev version, changelog

--- niworkflows/info.py
+++ niworkflows/info.py
@@ -16,7 +16,7 @@ import runpy
 nipype_info = runpy.run_path(op.join(op.abspath(op.dirname(__file__)),
                                      'nipype', 'info.py'))
 
-__version__ = '0.1.6-dev'
+__version__ = '0.1.7-dev'
 __packagename__ = 'niworkflows'
 __author__ = 'The CRN developers'
 __copyright__ = 'Copyright {}, Center for Reproducible Neuroscience, Stanford University'.format(
