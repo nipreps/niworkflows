df462c075b6c4b382bd2874873b9634be56f17b2
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2018-02-12 17:59:24 -0800

    removing if/else from circle scripts

--- .circle/pytests.sh
+++ .circle/pytests.sh
@@ -10,17 +10,8 @@ set -x         # Print command traces before executing command.
 
 code=0
 
-if [ "${CIRCLE_NODE_TOTAL:-1}" == "1" ]; then
-    docker run -ti --env SAVE_CIRCLE_ARTIFACTS="/scratch" -v ${SCRATCH}/py3:/scratch -w /scratch --entrypoint=/usr/local/miniconda/bin/py.test niworkflows:py3 /root/niworkflows -n ${CIRCLE_NPROCS:-4} -v --junit-xml=/scratch/pytest.xml
-    code=$(( $code + $? ))
-else
-    case ${CIRCLE_NODE_INDEX} in
-    0)
-        docker run --rm=false -ti --env SAVE_CIRCLE_ARTIFACTS="/scratch" --env CIRCLE_NPROCS=4 -v ${SCRATCH}/py3:/scratch -w /scratch --entrypoint=/usr/local/miniconda/bin/py.test niworkflows:py3 /root/niworkflows  -n 2 -v --junit-xml=/scratch/pytest.xml
-        code=$(( $code + $? ))
-        ;;
-    esac
-fi
+docker run -ti --env SAVE_CIRCLE_ARTIFACTS="/scratch" -v ${SCRATCH}/py3:/scratch -w /scratch --entrypoint=/usr/local/miniconda/bin/py.test niworkflows:py3 /root/niworkflows -n ${CIRCLE_NPROCS:-4} -v --junit-xml=/scratch/pytest.xml
+code=$(( $code + $? ))
 
 # Place xml files where appropriate
 cp $SCRATCH/py3/*.xml ${CIRCLE_TEST_REPORTS}/py3/  || true
