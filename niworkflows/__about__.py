fdf20103b9dedd4632bb8e26a5eb3b896e349fd2
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-09-04 16:29:55 -0400

    REL 0.4.3

--- niworkflows/__about__.py
+++ niworkflows/__about__.py
@@ -10,7 +10,7 @@ as well as for open-source software distribution.
 from __future__ import absolute_import, division, print_function
 import datetime
 
-__version__ = '0.4.3-dev'
+__version__ = '0.4.3'
 __packagename__ = 'niworkflows'
 __author__ = 'The CRN developers'
 __copyright__ = 'Copyright {}, Center for Reproducible Neuroscience, Stanford University'.format(
