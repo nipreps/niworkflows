e203a1898394b1cac93ca9d11038756c54d3404f
oesteban <code@oscaresteban.es>
2018-12-04 17:29:24 -0800

    project and merge aparcs, datasinks

--- niworkflows/anat/templateflow.py
+++ niworkflows/anat/templateflow.py
@@ -12,25 +12,30 @@ from __future__ import print_function, division, absolute_import, unicode_litera
 # general purpose
 import pkg_resources as pkgr
 import logging
-from multiprocessing import cpu_count
+from os import getenv, cpu_count
 
 # nipype
 from nipype import logging as nlogging
 from nipype.pipeline import engine as pe
 from nipype.interfaces import utility as niu
-from nipype.interfaces.ants import N4BiasFieldCorrection
+from nipype.interfaces import freesurfer as fs
+from nipype.interfaces.ants import N4BiasFieldCorrection, AntsJointFusion
+from nipype.interfaces.io import FreeSurferSource, DataSink
 
 # niworkflows
 from ..data import get_template
 from .ants import init_brain_extraction_wf, AI
 from ..interfaces.fixes import (
     FixHeaderRegistration as Registration,
-    # FixHeaderApplyTransforms as ApplyTransforms,
+    FixHeaderApplyTransforms as ApplyTransforms,
 )
+from ..interfaces.bids import DerivativesDataSink
 
 
 def init_templateflow_wf(
-    in_files,
+    bids_dir,
+    output_dir,
+    participant_label,
     mov_template,
     ref_template='MNI152NLin2009cAsym',
     use_float=True,
@@ -39,6 +44,7 @@ def init_templateflow_wf(
     modality='T1w',
     normalization_quality='precise',
     name='templateflow_wf',
+    fs_subjects_dir=getenv('SUBJECTS_DIR'),
 ):
     """
     A Nipype workflow to perform image registration between two templates
@@ -84,12 +90,16 @@ def init_templateflow_wf(
         tpl_mov_root / ('tpl-%s_space-MNI_res-01_%s.nii.gz' % (mov_template, modality)))
     tpl_mov_mask = str(
         tpl_mov_root / ('tpl-%s_space-MNI_res-01_brainmask.nii.gz' % mov_template))
-    ninputs = len(in_files)
+    ninputs = len(participant_label)
 
     wf = pe.Workflow(name)
-    inputnode = pe.Node(niu.IdentityInterface(fields=['in_files']),
+    inputnode = pe.Node(niu.IdentityInterface(fields=['participant_label']),
                         name='inputnode')
-    inputnode.iterables = ('in_files', in_files)
+    inputnode.iterables = ('participant_label', sorted(list(participant_label)))
+
+    pick_file = pe.Node(niu.Function(function=_bids_pick),
+                        name='pick_file', run_without_submitting=True)
+    pick_file.inputs.bids_root = bids_dir
 
     ref_bex = init_brain_extraction_wf(
         in_template=ref_template,
@@ -107,13 +117,12 @@ def init_templateflow_wf(
         name='moving_bex',
     )
 
-    ref_inu = pe.MapNode(
+    ref_inu = pe.Node(
         N4BiasFieldCorrection(
             dimension=3, save_bias=True, copy_header=True,
             n_iterations=[50] * 5, convergence_threshold=1e-7, shrink_factor=4,
             bspline_fitting_distance=200),
-        n_procs=omp_nthreads, name='ref_inu',
-        iterfield=['input_image', 'mask_image'])
+        n_procs=omp_nthreads, name='ref_inu')
 
     ref_norm = pe.MapNode(
         Registration(
@@ -190,10 +199,134 @@ def init_templateflow_wf(
         'OMP_NUM_THREADS': '%d' % omp_nthreads,
     }
 
+    fssource = pe.Node(
+        FreeSurferSource(subjects_dir=str(fs_subjects_dir)),
+        name='fssource', run_without_submitting=True)
+    tonative = pe.Node(fs.Label2Vol(), name='tonative')
+    tonii = pe.Node(
+        fs.MRIConvert(out_type='niigz', resample_type='nearest'),
+        name='tonii')
+
+    ref_aparc = pe.Node(
+        ApplyTransforms(interpolation='MultiLabel', float=True,
+                        reference_image=tpl_ref),
+        name='ref_aparc',
+        mem_gb=1
+    )
+
+    mov_aparc = pe.Node(
+        ApplyTransforms(interpolation='MultiLabel', float=True,
+                        reference_image=tpl_mov),
+        name='mov_aparc',
+        mem_gb=1
+    )
+
+    ref_aparc_buffer = pe.JoinNode(
+        niu.IdentityInterface(fields=['aparc']),
+        joinsource='inputnode', joinfield='aparc', name='ref_aparc_buffer')
+
+    ref_join_labels = pe.Node(
+        AntsJointFusion(
+            target_image=[tpl_ref],
+            out_label_fusion='merged_aparc.nii.gz',
+            out_intensity_fusion_name_format='merged_aparc_intensity_%d.nii.gz',
+            out_label_post_prob_name_format='merged_aparc_posterior_%d.nii.gz',
+            out_atlas_voting_weight_name_format='merged_aparc_weight_%d.nii.gz',
+        ),
+        name='ref_join_labels', n_procs=omp_nthreads)
+    ref_join_labels.inputs.environ = {
+        'NSLOTS': '%d' % omp_nthreads,
+        'ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS': '%d' % omp_nthreads,
+        'OMP_NUM_THREADS': '%d' % omp_nthreads,
+    }
+
+    ref_join_labels_ds = pe.Node(
+        DerivativesDataSink(
+            base_directory=str(output_dir.parent),
+            out_path_base=output_dir.name, space=ref_template,
+            suffix='dtissue', desc='aparc', keep_dtype=False,
+            source_file='group/tpl-{0}_T1w.nii.gz'.format(ref_template)),
+        name='ref_join_labels_ds', run_without_submitting=True)
+
+    # ref_join_probs_ds = pe.Node(
+    #     DerivativesDataSink(
+    #         base_directory=str(output_dir.parent),
+    #         out_path_base=output_dir.name, space=ref_template,
+    #         suffix='probtissue', desc='aparc', keep_dtype=False,
+    #         source_file='group/tpl-{0}_T1w.nii.gz'.format(ref_template)),
+    #     name='ref_join_probs_ds', run_without_submitting=True)
+
+    # ref_join_voting_ds = pe.Node(
+    #     DerivativesDataSink(
+    #         base_directory=str(output_dir.parent),
+    #         out_path_base=output_dir.name, space=ref_template,
+    #         suffix='probtissue', desc='aparcvoting', keep_dtype=False,
+    #         source_file='group/tpl-{0}_T1w.nii.gz'.format(ref_template)),
+    #     name='ref_join_voting_ds', run_without_submitting=True)
+
+    mov_aparc_buffer = pe.JoinNode(
+        niu.IdentityInterface(fields=['aparc']),
+        joinsource='inputnode', joinfield='aparc', name='mov_aparc_buffer')
+
+    mov_join_labels = pe.Node(
+        AntsJointFusion(
+            target_image=[tpl_mov],
+            out_label_fusion='merged_aparc.nii.gz',
+            out_intensity_fusion_name_format='merged_aparc_intensity_%d.nii.gz',
+            out_label_post_prob_name_format='merged_aparc_posterior_%d.nii.gz',
+            out_atlas_voting_weight_name_format='merged_aparc_weight_%d.nii.gz',
+        ),
+        name='mov_join_labels', n_procs=omp_nthreads)
+    mov_join_labels.inputs.environ = {
+        'NSLOTS': '%d' % omp_nthreads,
+        'ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS': '%d' % omp_nthreads,
+        'OMP_NUM_THREADS': '%d' % omp_nthreads,
+    }
+
+    mov_join_labels_ds = pe.Node(
+        DerivativesDataSink(
+            base_directory=str(output_dir.parent),
+            out_path_base=output_dir.name, space=mov_template,
+            suffix='dtissue', desc='aparc', keep_dtype=False,
+            source_file='group/tpl-{0}_T1w.nii.gz'.format(mov_template)),
+        name='mov_join_labels_ds', run_without_submitting=True)
+
+    # Datasinking
+    ref_norm_ds = pe.Node(
+        DerivativesDataSink(base_directory=str(output_dir.parent),
+                            out_path_base=output_dir.name, space=ref_template,
+                            desc='preproc', keep_dtype=True),
+        name='ref_norm_ds', run_without_submitting=True
+    )
+
+    mov_norm_ds = pe.Node(
+        DerivativesDataSink(base_directory=str(output_dir.parent),
+                            out_path_base=output_dir.name, space=mov_template,
+                            desc='preproc', keep_dtype=True),
+        name='mov_norm_ds', run_without_submitting=True
+    )
+
+    ref_aparc_ds = pe.Node(
+        DerivativesDataSink(base_directory=str(output_dir.parent),
+                            out_path_base=output_dir.name, space=ref_template,
+                            suffix='dtissue', desc='aparc', keep_dtype=False),
+        name='ref_aparc_ds', run_without_submitting=True
+    )
+
+    mov_aparc_ds = pe.Node(
+        DerivativesDataSink(base_directory=str(output_dir.parent),
+                            out_path_base=output_dir.name, space=mov_template,
+                            suffix='dtissue', desc='aparc', keep_dtype=False),
+        name='mov_aparc_ds', run_without_submitting=True
+    )
+
     wf.connect([
-        (inputnode, ref_bex, [('in_files', 'inputnode.in_files')]),
-        (inputnode, mov_bex, [('in_files', 'inputnode.in_files')]),
-        (inputnode, ref_inu, [('in_files', 'input_image')]),
+        (inputnode, pick_file, [('participant_label', 'participant_label')]),
+        (inputnode, fssource, [(('participant_label', _sub_decorate), 'subject_id')]),
+        (pick_file, ref_bex, [('out', 'inputnode.in_files')]),
+        (pick_file, mov_bex, [('out', 'inputnode.in_files')]),
+        (pick_file, ref_inu, [('out', 'input_image')]),
+        (pick_file, tonii, [('out', 'reslice_like')]),
         (ref_bex, ref_inu, [('outputnode.out_mask', 'mask_image')]),
         (ref_inu, ref_norm, [('output_image', 'moving_image')]),
         (ref_bex, ref_norm, [('outputnode.out_mask', 'moving_image_masks'),
@@ -206,13 +339,64 @@ def init_templateflow_wf(
         (mov_norm, mov_buffer, [('warped_image', 'moving_image')]),
         (ref_buffer, flow, [(('fixed_image', _flatten), 'fixed_image')]),
         (mov_buffer, flow, [(('moving_image', _flatten), 'moving_image')]),
-
+        # Select DKT aparc
+        (fssource, tonative, [(('aparc_aseg', _last), 'seg_file'),
+                              ('rawavg', 'template_file'),
+                              ('aseg', 'reg_header')]),
+        (tonative, tonii, [('vol_label_file', 'in_file')]),
+        (tonii, ref_aparc, [('out_file', 'input_image')]),
+        (tonii, mov_aparc, [('out_file', 'input_image')]),
+        (ref_norm, ref_aparc, [('composite_transform', 'transforms')]),
+        (mov_norm, mov_aparc, [('composite_transform', 'transforms')]),
+        (ref_buffer, ref_join_labels, [
+            (('fixed_image', _flatten), 'atlas_image')]),
+        (ref_aparc, ref_aparc_buffer, [('output_image', 'aparc')]),
+        (ref_aparc_buffer, ref_join_labels, [
+            ('aparc', 'atlas_segmentation_image')]),
+        (mov_buffer, mov_join_labels, [
+            (('moving_image', _flatten), 'atlas_image')]),
+        (mov_aparc, mov_aparc_buffer, [('output_image', 'aparc')]),
+        (mov_aparc_buffer, mov_join_labels, [
+            ('aparc', 'atlas_segmentation_image')]),
+        # Datasinks
+        (ref_join_labels, ref_join_labels_ds, [('out_label_fusion', 'in_file')]),
+        # (ref_join_labels, ref_join_probs_ds, [
+        #     ('out_label_post_prob_name_format', 'in_file')]),
+        # (ref_join_labels, ref_join_voting_ds, [
+        #     ('out_atlas_voting_weight_name_format', 'in_file')]),
+        (mov_join_labels, mov_join_labels_ds, [('out_label_fusion', 'in_file')]),
+        (pick_file, ref_norm_ds, [('out', 'source_file')]),
+        (ref_norm, ref_norm_ds, [('warped_image', 'in_file')]),
+        (pick_file, mov_norm_ds, [('out', 'source_file')]),
+        (mov_norm, mov_norm_ds, [('warped_image', 'in_file')]),
+        (pick_file, ref_aparc_ds, [('out', 'source_file')]),
+        (ref_aparc, ref_aparc_ds, [('output_image', 'in_file')]),
+        (pick_file, mov_aparc_ds, [('out', 'source_file')]),
+        (mov_aparc, mov_aparc_ds, [('output_image', 'in_file')]),
     ])
     return wf
 
 
+def _last(inlist):
+    return inlist[-1]
+
+
 def _flatten(inlist):
     return [item for sublist in inlist for item in sublist]
+
+
+def _extract_id(in_file):
+    from pathlib import Path
+    return Path(in_file).name.split('_')[0]
+
+
+def _bids_pick(participant_label, bids_root):
+    return str(bids_root / ('sub-%s' % participant_label) / 'anat' /
+               ('sub-%s_T1w.nii.gz' % participant_label))
+
+
+def _sub_decorate(label):
+    return "sub-%s" % label
 
 
 def cli():
@@ -222,7 +406,11 @@ def cli():
     from argparse import RawTextHelpFormatter
 
     parser = ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
-    parser.add_argument('input_files', nargs='+', action='store', help='the input file',
+    parser.add_argument('bids_dir', action='store', help='BIDS directory root',
+                        type=Path)
+    parser.add_argument('--participant-label', nargs='*', action='store',
+                        help='list of participants to be processed')
+    parser.add_argument('--output-dir', action='store', help='output directory',
                         type=Path)
     parser.add_argument('--reference', '-r', action='store', help='the reference template')
     parser.add_argument('--moving', '-m', action='store', help='the moving template')
@@ -236,6 +424,11 @@ def cli():
                         help="increases log verbosity for each occurence, debug level is -vvv")
     parser.add_argument('--legacy', action='store_true', default=False,
                         help='use LegacyMultiProc')
+    parser.add_argument('-w', '--work-dir', action='store', type=Path,
+                        default=Path() / 'work', help='work directory')
+    parser.add_argument('--freesurfer', action='store', type=Path,
+                        help='path to precomputed freesurfer results',
+                        default=Path(getenv('SUBJECTS_DIR', 'freesurfer/')).resolve())
     opts = parser.parse_args()
 
     plugin_settings = {'plugin': 'Linear'}
@@ -256,10 +449,45 @@ def cli():
     nlogging.getLogger('nipype.interface').setLevel(log_level)
     nlogging.getLogger('nipype.utils').setLevel(log_level)
 
+    bids_dir = opts.bids_dir.resolve()
+    if not bids_dir.exists():
+        raise RuntimeError(
+            'Input BIDS directory "%s" does not exist.' % bids_dir)
+
+    if not opts.output_dir:
+        output_dir = bids_dir / 'derivatives' / 'templateflow_0.0.1'
+    else:
+        output_dir = opts.output_dir.resolve()
+
+    all_subjects = set([Path(s).name.split('_')[0].split('-')[-1]
+                        for s in bids_dir.glob('sub-*')])
+
+    if not all_subjects:
+        raise RuntimeError(
+            'Input BIDS directory "%s" does not look like a valid BIDS '
+            'tree.' % bids_dir)
+
+    if not opts.participant_label:
+        participants = all_subjects
+    else:
+        part_label = [s[4:] if s.startswith('sub-') else s
+                      for s in opts.participant_label]
+        participants = all_subjects.intersection(part_label)
+
+    if not participants:
+        raise RuntimeError(
+            'No subject with the specified participant labels (%s) matched.' %
+            ', '.join(opts.participant_label))
+
     tf = init_templateflow_wf(
-        [[str(f)] for f in opts.input_files],
-        opts.moving, ref_template=opts.reference,
+        bids_dir,
+        output_dir,
+        participants,
+        opts.moving,
+        ref_template=opts.reference,
         omp_nthreads=opts.omp_nthreads,
-        normalization_quality='precise' if not opts.testing else 'testing')
-    tf.base_dir = str(Path().resolve())
+        normalization_quality='precise' if not opts.testing else 'testing',
+        fs_subjects_dir=opts.freesurfer,
+    )
+    tf.base_dir = str(opts.work_dir.resolve())
     tf.run(**plugin_settings)
---
+++ niworkflows/interfaces/bids.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Interfaces for handling BIDS-like neuroimaging structures
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Fetch some example data:
+    >>> import os
+    >>> from niworkflows import data
+    >>> data_root = data.get_bids_examples(variant='BIDS-examples-1-enh-ds054')
+    >>> os.chdir(data_root)
+Disable warnings:
+    >>> from nipype import logging
+    >>> logging.getLogger('nipype.interface').setLevel('ERROR')
+"""
+
+from pathlib import Path
+import os
+import re
+import gzip
+from shutil import copyfileobj
+
+from nipype import logging
+from nipype.interfaces.base import (
+    isdefined, TraitedSpec, BaseInterfaceInputSpec,
+    SimpleInterface, traits)
+from nipype.interfaces.base.traits_extension import (
+    File, Directory, InputMultiPath, OutputMultiPath, Str
+)
+from nipype.utils.filemanip import copyfile
+
+LOGGER = logging.getLogger('nipype.interface')
+BIDS_NAME = re.compile(
+    r'^(.*\/)?'
+    '(?P<subject_id>(sub|tpl)-[a-zA-Z0-9]+)'
+    '(_(?P<session_id>ses-[a-zA-Z0-9]+))?'
+    '(_(?P<task_id>task-[a-zA-Z0-9]+))?'
+    '(_(?P<acq_id>acq-[a-zA-Z0-9]+))?'
+    '(_(?P<rec_id>rec-[a-zA-Z0-9]+))?'
+    '(_(?P<run_id>run-[a-zA-Z0-9]+))?')
+
+
+class DerivativesDataSinkInputSpec(BaseInterfaceInputSpec):
+    base_directory = Directory(
+        desc='Path to the base directory for storing data.')
+    in_file = InputMultiPath(File(exists=True), mandatory=True,
+                             desc='the object to be saved')
+    source_file = File(exists=False, mandatory=True, desc='the input func file')
+    space = Str('', usedefault=True, desc='Label for space field')
+    desc = Str('', usedefault=True, desc='Label for description field')
+    suffix = Str('', usedefault=True, desc='suffix appended to source_file')
+    keep_dtype = traits.Bool(False, usedefault=True, desc='keep datatype suffix')
+    extra_values = traits.List(Str)
+    compress = traits.Bool(desc="force compression (True) or uncompression (False)"
+                                " of the output file (default: same as input)")
+
+
+class DerivativesDataSinkOutputSpec(TraitedSpec):
+    out_file = OutputMultiPath(File(exists=True, desc='written file path'))
+    compression = OutputMultiPath(
+        traits.Bool, desc='whether ``in_file`` was compressed/uncompressed '
+                          'or `it was copied directly.')
+
+
+class DerivativesDataSink(SimpleInterface):
+    """
+    Saves the `in_file` into a BIDS-Derivatives folder provided
+    by `base_directory`, given the input reference `source_file`.
+    >>> from pathlib import Path
+    >>> import tempfile
+    >>> from fmriprep.utils.bids import collect_data
+    >>> tmpdir = Path(tempfile.mkdtemp())
+    >>> tmpfile = tmpdir / 'a_temp_file.nii.gz'
+    >>> tmpfile.open('w').close()  # "touch" the file
+    >>> dsink = DerivativesDataSink(base_directory=str(tmpdir))
+    >>> dsink.inputs.in_file = str(tmpfile)
+    >>> dsink.inputs.source_file = collect_data('ds114', '01')[0]['t1w'][0]
+    >>> dsink.inputs.keep_dtype = True
+    >>> dsink.inputs.suffix = 'target-mni'
+    >>> res = dsink.run()
+    >>> res.outputs.out_file  # doctest: +ELLIPSIS
+    '.../fmriprep/sub-01/ses-retest/anat/sub-01_ses-retest_target-mni_T1w.nii.gz'
+    >>> bids_dir = tmpdir / 'bidsroot' / 'sub-02' / 'ses-noanat' / 'func'
+    >>> bids_dir.mkdir(parents=True, exist_ok=True)
+    >>> tricky_source = bids_dir / 'sub-02_ses-noanat_task-rest_run-01_bold.nii.gz'
+    >>> tricky_source.open('w').close()
+    >>> dsink = DerivativesDataSink(base_directory=str(tmpdir))
+    >>> dsink.inputs.in_file = str(tmpfile)
+    >>> dsink.inputs.source_file = str(tricky_source)
+    >>> dsink.inputs.keep_dtype = True
+    >>> dsink.inputs.desc = 'preproc'
+    >>> res = dsink.run()
+    >>> res.outputs.out_file  # doctest: +ELLIPSIS
+    '.../fmriprep/sub-02/ses-noanat/func/sub-02_ses-noanat_task-rest_run-01_\
+desc-preproc_bold.nii.gz'
+    """
+    input_spec = DerivativesDataSinkInputSpec
+    output_spec = DerivativesDataSinkOutputSpec
+    out_path_base = "fmriprep"
+    _always_run = True
+
+    def __init__(self, out_path_base=None, **inputs):
+        super(DerivativesDataSink, self).__init__(**inputs)
+        self._results['out_file'] = []
+        if out_path_base:
+            self.out_path_base = out_path_base
+
+    def _run_interface(self, runtime):
+        src_fname, _ = _splitext(self.inputs.source_file)
+        src_fname, dtype = src_fname.rsplit('_', 1)
+        _, ext = _splitext(self.inputs.in_file[0])
+        if self.inputs.compress is True and not ext.endswith('.gz'):
+            ext += '.gz'
+        elif self.inputs.compress is False and ext.endswith('.gz'):
+            ext = ext[:-3]
+
+        m = BIDS_NAME.search(src_fname)
+
+        mod = Path(self.inputs.source_file).parent.name
+
+        base_directory = runtime.cwd
+        if isdefined(self.inputs.base_directory):
+            base_directory = self.inputs.base_directory
+
+        base_directory = Path(base_directory).resolve()
+        out_path = base_directory / self.out_path_base / \
+            '{subject_id}'.format(**m.groupdict())
+
+        if m.groupdict().get('session_id') is not None:
+            out_path = out_path / '{session_id}'.format(**m.groupdict())
+
+        out_path = out_path / '{}'.format(mod)
+        out_path.mkdir(exist_ok=True, parents=True)
+        base_fname = str(out_path / src_fname)
+
+        formatstr = '{bname}{space}{desc}{suffix}{dtype}{ext}'
+        if len(self.inputs.in_file) > 1 and not isdefined(self.inputs.extra_values):
+            formatstr = '{bname}{space}{desc}{suffix}{i:04d}{dtype}{ext}'
+
+        space = '_space-{}'.format(self.inputs.space) if self.inputs.space else ''
+        desc = '_desc-{}'.format(self.inputs.desc) if self.inputs.desc else ''
+        suffix = '_{}'.format(self.inputs.suffix) if self.inputs.suffix else ''
+        dtype = '' if not self.inputs.keep_dtype else ('_%s' % dtype)
+
+        self._results['compression'] = []
+        for i, fname in enumerate(self.inputs.in_file):
+            out_file = formatstr.format(
+                bname=base_fname,
+                space=space,
+                desc=desc,
+                suffix=suffix,
+                i=i,
+                dtype=dtype,
+                ext=ext)
+            if isdefined(self.inputs.extra_values):
+                out_file = out_file.format(extra_value=self.inputs.extra_values[i])
+            self._results['out_file'].append(out_file)
+            self._results['compression'].append(_copy_any(fname, out_file))
+        return runtime
+
+
+def _splitext(fname):
+    basename = Path(fname).name
+    ext = Path(basename).suffix if not basename.endswith('.gz') else \
+        ''.join(Path(basename).suffixes[-2:])
+    return basename[: -len(ext)], ext
+
+
+def _copy_any(src, dst):
+    src_isgz = src.endswith('.gz')
+    dst_isgz = dst.endswith('.gz')
+    if src_isgz == dst_isgz:
+        copyfile(src, dst, copy=True, use_hardlink=True)
+        return False  # Make sure we do not reuse the hardlink later
+
+    # Unlink target (should not exist)
+    if os.path.exists(dst):
+        os.unlink(dst)
+
+    src_open = gzip.open if src_isgz else open
+    dst_open = gzip.open if dst_isgz else open
+    with src_open(src, 'rb') as f_in:
+        with dst_open(dst, 'wb') as f_out:
+            copyfileobj(f_in, f_out)
+    return True
