5d1f748830840713613cc88e7b42ab92bc2f81b6
rciric <rc042010@mymail.pomona.edu>
2019-01-27 18:12:25 -0800

    fix case conversion

--- niworkflows/interfaces/utils.py
+++ niworkflows/interfaces/utils.py
@@ -813,7 +813,7 @@ def _tsv2json(in_tsv, out_json, index_column, additional_metadata=None,
     tsv_data.set_index(index_column, drop=True, inplace=True)
     if enforce_case:
         tsv_data.index = [re.sub(re_to_snake, snake,
-                                 less_breakable(i).lower(), 0)
+                                 less_breakable(i), 0).lower()
                           for i in tsv_data.index]
         tsv_data.columns = [re.sub(re_to_camel, camel,
                                    less_breakable(i).title(), 0)
