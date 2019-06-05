36f2b562542635089d422d91f69fc9123b4abd31
Oscar Esteban <code@oscaresteban.es>
2019-06-04 22:10:12 -0700

    PIN: templateflow to latest version 0.3.0, including infant and pediatric templates

--- niworkflows/__about__.py
+++ niworkflows/__about__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """
@@ -64,7 +63,7 @@ REQUIRES = [
     'scipy',
     'seaborn',
     'svgutils',
-    'templateflow<0.2.0a0,>=0.1.7',
+    'templateflow<0.4.0a0,>=0.3.0',
 ]
 
 SETUP_REQUIRES = []
