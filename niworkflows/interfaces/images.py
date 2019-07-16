032483af43f790d7f2f40ca85862b2e7a9dd912f
Ursula Tooley <utooley@gmail.com>
2019-07-16 16:43:22 -0400

    Update niworkflows/interfaces/images.py
    Co-Authored-By: Oscar Esteban <code@oscaresteban.es>

--- niworkflows/interfaces/images.py
+++ niworkflows/interfaces/images.py
@@ -402,7 +402,7 @@ class ValidateImage(SimpleInterface):
                 self._results['out_report'] = out_report
                 return runtime
             diff = np.linalg.inv(qform) @ new_qform
-            trans, rot, scale, shear = transforms3d.affines.decompose44(diff)
+            trans, rot, scale, _ = transforms3d.affines.decompose44(diff)
             axis, angle = transforms3d.axangles.mat2axangle(rot)
             total_trans = np.sqrt(np.sum(trans * trans)) # Add angle and total_trans to report
             warning_txt = 'Note on orientation: qform matrix overwritten'
