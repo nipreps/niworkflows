995be791660066eb43c9c975ab81642f0b8e2ec4
David Hunt <davhunt@indiana.edu>
2019-09-11 14:56:30 -0400

    add check_lta_length function

--- niworkflows/interfaces/freesurfer.py
+++ niworkflows/interfaces/freesurfer.py
@@ -204,20 +204,7 @@ class TruncateLTA(object):
             lta_file = outputs[lta_name]
             if not isdefined(lta_file):
                 continue
-
-            lines = Path(lta_file).read_text().splitlines(keepends=True)
-
-            fixed = False
-            newfile = []
-            for line in lines:
-                if line.startswith('filename = ') and len(line.strip("\n")) >= 255:
-                    fixed = True
-                    newfile.append('filename = path_too_long\n')
-                else:
-                    newfile.append(line)
-
-            if fixed:
-                Path(lta_file).write_text(''.join(newfile))
+            check_lta_length(lta_file)
 
         runtime = super(TruncateLTA, self)._post_run_hook(runtime)
         return runtime
@@ -324,6 +311,22 @@ class MedialNaNs(SimpleInterface):
             self.inputs.target_subject,
             newpath=runtime.cwd)
         return runtime
+
+
+def check_lta_length(lta_file):
+    lines = Path(lta_file).read_text().splitlines(keepends=True)
+
+    fixed = False
+    newfile = []
+    for line in lines:
+        if line.startswith('filename = ') and len(line.strip("\n")) >= 255:
+            fixed = True
+            newfile.append('filename = path_too_long\n')
+        else:
+            newfile.append(line)
+
+    if fixed:
+        Path(lta_file).write_text(''.join(newfile))
 
 
 def inject_skullstripped(subjects_dir, subject_id, skullstripped):
