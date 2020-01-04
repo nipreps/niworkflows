b885ba132dbacf29071fd55cf846d0ef19feb3af
James Kent <james-kent@uiowa.edu>
2020-01-04 00:22:30 -0600

    add new tests and test data

---
+++ niworkflows/reports/tests/test_core.py
@@ -0,0 +1,51 @@
+''' Testing module for niworkflows.reports.core '''
+
+import os
+from pathlib import Path
+from pkg_resources import resource_filename
+import tempfile
+
+import pytest
+
+from ..core import Report
+
+
+@pytest.fixture
+def test_report():
+    test_data_path = resource_filename(
+        'niworkflows',
+        os.path.join('data', 'tests', 'work', 'reportlets'))
+    out_dir = tempfile.mkdtemp()
+
+    return Report(Path(test_data_path), Path(out_dir), 'fakeiuud',
+                  subject_id='01', packagename='fmriprep')
+
+
+@pytest.mark.parametrize(
+    "orderings,expected_entities,expected_value_combos",
+    [
+        (['session', 'task', 'run'],
+         ['task', 'run'],
+         [
+            ('faketask', None),
+            ('mixedgamblestask', 1),
+            ('mixedgamblestask', 2),
+            ('mixedgamblestask', 3),
+        ]),
+        (['run', 'task', 'session'],
+         ['run', 'task'],
+         [
+            (None, 'faketask'),
+            (1, 'mixedgamblestask'),
+            (2, 'mixedgamblestask'),
+            (3, 'mixedgamblestask'),
+        ])
+    ]
+)
+def test_process_orderings(test_report, orderings,
+                           expected_entities, expected_value_combos):
+    test_report.init_layout()
+    entities, value_combos = test_report._process_orderings(orderings, test_report.layout)
+
+    assert entities == expected_entities
+    assert expected_value_combos == value_combos
