9b7d02b72b8ff280b6a658af22d7a800e5e733aa
mathiasg <mathiasg@stanford.edu>
2020-02-03 16:45:19 -0500

    ENH: add BIDS output version checker

---
+++ niworkflows/utils/tests/test_bids.py
@@ -0,0 +1,15 @@
+import json
+
+import pytest
+from ..bids import check_pipeline_version
+
+
+def test_check_pipeline_version(tmp_path):
+    data = {"PipelineDescription": {"Version": "1.1.1"}}
+    desc = tmp_path / 'dataset_description.json'
+    with open(desc, 'wt') as fp:
+        json.dump(data, fp)
+
+    check_pipeline_version('1.1.1', str(desc))
+    with pytest.warns(UserWarning):
+        check_pipeline_version('1.2', desc)
