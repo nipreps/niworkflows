5e3c93c00c497ce99795a59176e6da382ca8ed11
Derek Pisner <16432683+dPys@users.noreply.github.com>
2020-03-24 16:56:40 -0500

    Update niworkflows/interfaces/nibabel.py
    Co-Authored-By: Chris Markiewicz <effigies@gmail.com>

--- niworkflows/interfaces/nibabel.py
+++ niworkflows/interfaces/nibabel.py
@@ -7,7 +7,7 @@ from nipype import logging
 from nipype.utils.filemanip import fname_presuffix
 from nipype.interfaces.base import (
     traits, TraitedSpec, BaseInterfaceInputSpec, File,
-    SimpleInterface, OutputMultiPath, InputMultiPath
+    SimpleInterface, OutputMultiObject, InputMultiObject
 )
 
 IFLOGGER = logging.getLogger('nipype.interface')
