ee4f2325f4dd9eaa01ce9c4bff0c8dea8925a365
oesteban <code@oscaresteban.es>
2020-05-01 22:56:46 -0700

    fix(packaging): report.tpl not being found by fmriprep
    there seems to be a conflict with setuptools-scm, which is a dependency
    to sdcflows.
    
    setuptools-scm includes all files tracked by git by default, completely
    altering the functioning of the ``MANIFEST.in`` file and the
    ``setup.cfg`` metadata regarding data and modules.

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-""" niworkflows setup script """
+"""niworkflows setup script."""
 import sys
 from setuptools import setup
 import versioneer
 
-# Give setuptools a hint to complain if it's too old a version
-# 30.3.0 allows us to put most metadata in setup.cfg
-# 30.4.0 gives us options.packages.find
-# Should match pyproject.toml
-SETUP_REQUIRES = ['setuptools >= 30.4.0']
-# This enables setuptools to install wheel on-the-fly
-SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
-
 if __name__ == '__main__':
-    setup(name='niworkflows',
-          version=versioneer.get_version(),
-          cmdclass=versioneer.get_cmdclass(),
-          setup_requires=SETUP_REQUIRES,
-          )
+    setupargs = {
+        "version": versioneer.get_version(),
+        "cmdclass": versioneer.get_cmdclass(),
+    }
+    if "bdist_wheel" in sys.argv:
+        setupargs["setup_requires"] = ["setuptools >= 38.3.0", "wheel"]
+    setup(**setupargs)
