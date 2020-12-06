66ce8d515d0ae79c520ce1cad5c9102c5730d745
Oscar Esteban <code@oscaresteban.es>
2020-12-06 15:27:48 +0100

    MAINT: Migrate from Travis -> GH Actions [backport 1.3.x]

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-""" niworkflows setup script """
+"""niworkflows setup script."""
 import sys
 from setuptools import setup
 import versioneer
@@ -13,16 +13,18 @@ import versioneer
 # To install, 30.4.0 is enough, but if we're building an sdist, require 40.8.0
 # This imposes a stricter rule on the maintainer than the user
 # Keep the installation version synchronized with pyproject.toml
-SETUP_REQUIRES = ['setuptools >= %s' % ("40.8.0" if "sdist" in sys.argv else "30.4.0")]
+SETUP_REQUIRES = [f"setuptools >= {'40.8.0' if 'sdist' in sys.argv else '30.4.0'}"]
 
 # This enables setuptools to install wheel on-the-fly
-SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
+if "bdist_wheel" in sys.argv:
+    SETUP_REQUIRES += ["wheel"]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Note that "name" is used by GitHub to determine what repository provides a package
     # in building its dependency graph.
-    setup(name='niworkflows',
-          version=versioneer.get_version(),
-          cmdclass=versioneer.get_cmdclass(),
-          setup_requires=SETUP_REQUIRES,
-          )
+    setup(
+        name="niworkflows",
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        setup_requires=SETUP_REQUIRES,
+    )
