3ab0ccc1ded0c6c45a2912cb4bfc4c7562d687af
Christopher J. Markiewicz <markiewicz@stanford.edu>
2021-10-01 10:26:03 -0400

    FIX: Import ROIsPlot from the correct location [test masks]

--- niworkflows/func/tests/test_util.py
+++ niworkflows/func/tests/test_util.py
@@ -9,7 +9,7 @@ from nipype.pipeline import engine as pe
 from nipype.utils.filemanip import fname_presuffix, copyfile
 from nilearn.image import load_img
 
-from ...interfaces.reportlets.masks import ROIsPlot
+from ...interfaces.masks import ROIsPlot
 
 from ..util import init_bold_reference_wf
 
