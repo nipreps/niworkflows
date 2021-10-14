6d9b84683b81cf0af175e3705449768c73d8eaf8
Oscar Esteban <code@oscaresteban.es>
2021-10-14 10:09:11 +0200

    enh: final touches to documentation build process

--- docs/_static/js/version-switch.js
+++ docs/_static/js/version-switch.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
     var pathname = window.location.pathname;
     var cur_ver = $("#version-slug").text().replace(/^[\n\s]+|[\n\s]+$/g, '')
-    if ( cur_ver.endsWith(" (dev)") ) {
-        cur_ver = "master";
+    var major_minor = "master";
+    if ( cur_ver.lastIndexOf(" (dev)") == -1 ) {
+        major_minor = `${cur_ver.split('.')[0]}.${cur_ver.split('.')[1]}`
     }
-    var relpath = pathname.substring(pathname.indexOf(cur_ver)).replace(/\/$/, '');
+    var relpath = pathname.substring(pathname.lastIndexOf(major_minor)).replace(/\/$/, '');
     var levels = relpath.split("/").length - 1
     if ( levels == 0 ) {
         levels = 1
-	    relpath += "/"
+        relpath += "/"
     }
     var versions_file = "../".repeat(levels) + "versions.json"
     relpath = "../".repeat(levels) + relpath
@@ -19,9 +20,19 @@ $(document).ready(function() {
         $("#version-slug").remove();  // Unnecessary if JSON was downloaded
 
         $.each(data["tags"].reverse(), function( i, val ) {
-            var new_path = relpath.replace(cur_ver, val)
+            var new_path = relpath.replace(major_minor, val)
             var item = `<li class="toctree-l2"><a class="reference internal" href="${new_path}">${val}</a></li>`
-            console.log(item)
+            if ( i == 0 ) {
+                item = `<li class="toctree-l2"><a class="reference internal" href="${new_path}">${val} (Latest Release)</a></li>`
+            }
+            $("#v-tags").append(item)
+        });
+        $.each(data["heads"].reverse(), function( i, val ) {
+            var new_path = relpath.replace(major_minor, val)
+            var item = `<li class="toctree-l2"><a class="reference internal" href="${new_path}">${val}</a></li>`
+            if ( val == "master" ) {
+                item = `<li class="toctree-l2"><a class="reference internal" href="${new_path}">${val} (Development)</a></li>`
+            }
             $("#v-tags").append(item)
         });
     }).fail(function() {
