2fb556a18bd26635d5dbf5861f565f5a2f388fb6
mathiasg <mathiasg@stanford.edu>
2022-02-03 13:44:53 -0500

    ENH: Add interface to merge across ROIs

--- niworkflows/interfaces/nibabel.py
+++ niworkflows/interfaces/nibabel.py
@@ -224,6 +224,25 @@ class MergeSeries(SimpleInterface):
         return runtime
 
 
+class MergeROIsInputSpec(BaseInterfaceInputSpec):
+    in_files = InputMultiObject(File(exists=True), desc="ROI files to be merged")
+
+
+class MergeROIsOutputSpec(TraitedSpec):
+    out_file = File(exists=True, desc="NIfTI containing all ROIs")
+
+
+class MergeROIs(SimpleInterface):
+    """Combine multiple region of interest files (3D or 4D) into a single file"""
+
+    input_spec = MergeROIsInputSpec
+    output_spec = MergeROIsOutputSpec
+
+    def _run_interface(self, runtime):
+        self._results["out_file"] = _merge_rois(self.inputs.in_files, newpath=runtime.cwd)
+        return runtime
+
+
 class _RegridToZoomsInputSpec(BaseInterfaceInputSpec):
     in_file = File(
         exists=True, mandatory=True, desc="a file whose resolution is to change"
@@ -600,4 +619,42 @@ def _dilate(in_file, radius=3, iterations=1, newpath=None):
     hdr.set_data_dtype("uint8")
     out_file = fname_presuffix(in_file, suffix="_dil", newpath=newpath or Path.cwd())
     mask.__class__(newdata.astype("uint8"), mask.affine, hdr).to_filename(out_file)
+    return out_file
+
+
+def _merge_rois(in_files, newpath=None):
+    """
+    Aggregate individual 4D ROI files together into a single subcortical NIfTI.
+    All ROI images are sanity checked with regards to:
+    1) Shape
+    2) Affine
+    3) Overlap
+
+    If any of these checks fail, an ``AssertionError`` will be raised.
+    """
+    from pathlib import Path
+    import nibabel as nb
+    import numpy as np
+
+    img = nb.load(in_files[0])
+    data = np.array(img.dataobj)
+    affine = img.affine
+    header = img.header
+
+    nonzero = np.any(data, axis=3)
+    for roi in in_files[1:]:
+        img = nb.load(roi)
+        assert img.shape == data.shape, "Mismatch in image shape"
+        assert np.allclose(img.affine, affine), "Mismatch in affine"
+        roi_data = np.asanyarray(img.dataobj)
+        roi_nonzero = np.any(roi_data, axis=3)
+        assert not np.any(roi_nonzero & nonzero), "Overlapping ROIs"
+        nonzero |= roi_nonzero
+        data += roi_data
+        del roi_data
+
+    if newpath is None:
+        newpath = Path()
+    out_file = str((Path(newpath) / "combined.nii.gz").absolute())
+    img.__class__(data, affine, header).to_filename(out_file)
     return out_file
--- niworkflows/interfaces/tests/test_nibabel.py
+++ niworkflows/interfaces/tests/test_nibabel.py
@@ -22,11 +22,77 @@
 #
 """test nibabel interfaces."""
 import os
+import uuid
 import numpy as np
 import nibabel as nb
 import pytest
 
-from ..nibabel import Binarize, ApplyMask, SplitSeries, MergeSeries
+from ..nibabel import Binarize, ApplyMask, SplitSeries, MergeSeries, MergeROIs
+
+
+@pytest.fixture
+def create_roi(tmp_path):
+    files = []
+
+    def _create_roi(affine, img_data, roi_index):
+        img_data[tuple(roi_index)] = 1
+        nii = nb.Nifti1Image(img_data, affine)
+        filename = tmp_path / f"{str(uuid.uuid4())}.nii.gz"
+        files.append(filename)
+        nii.to_filename(filename)
+        return filename
+    yield _create_roi
+    # cleanup files
+    for f in files:
+        f.unlink()
+
+
+# create a slightly off affine
+bad_affine = np.eye(4)
+bad_affine[0, -1] = -1
+
+
+@pytest.mark.parametrize(
+    "affine, data, roi_index, error, err_message",
+    [
+        (np.eye(4), np.zeros((2, 2, 2, 2), dtype=int), [1, 0], None, None),
+        (
+            np.eye(4),
+            np.zeros((2, 2, 3, 2), dtype=int),
+            [1, 0],
+            True,
+            "Mismatch in image shape",
+        ),
+        (
+            bad_affine,
+            np.zeros((2, 2, 2, 2), dtype=int),
+            [1, 0],
+            True,
+            "Mismatch in affine",
+        ),
+        (
+            np.eye(4),
+            np.zeros((2, 2, 2, 2), dtype=int),
+            [0, 0, 0],
+            True,
+            "Overlapping ROIs",
+        ),
+    ],
+)
+def test_merge_rois(tmpdir, create_roi, affine, data, roi_index, error, err_message):
+    tmpdir.chdir()
+    roi0 = create_roi(np.eye(4), np.zeros((2, 2, 2, 2), dtype=int), [0, 0])
+    roi1 = create_roi(np.eye(4), np.zeros((2, 2, 2, 2), dtype=int), [0, 1])
+    test_roi = create_roi(affine, data, roi_index)
+
+    merge = MergeROIs(in_files=[roi0, roi1, test_roi])
+    if error is None:
+        merge.run()
+        return
+    # otherwise check expected exceptions
+    with pytest.raises(AssertionError) as err:
+        merge.run()
+    assert err_message in str(err.value)
 
 
 def test_Binarize(tmp_path):
