481da9d9cde9e6ee419f1ea0c46e714320e33f68
celprov <77437752+celprov@users.noreply.github.com>
2022-03-02 12:57:41 +0100

    enh : make the morphology interface CrownMask more general
    Co-authored-by: Oscar Esteban <code@oscaresteban.es>

--- niworkflows/interfaces/morphology.py
+++ niworkflows/interfaces/morphology.py
@@ -1,7 +1,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 #
-# Copyright 2021 The NiPreps Developers <nipreps@gmail.com>
+# Copyright 2022 The NiPreps Developers <nipreps@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
 #
 #     https://www.nipreps.org/community/licensing/
 #
-""" Handling brain mask"""
+"""Mathematical morphology operations as nipype interfaces."""
 
 from nipype.interfaces.base import (
     traits,
@@ -31,23 +31,20 @@ from nipype.interfaces.base import (
 )
 
 
-class _CrownMaskInputSpec(BaseInterfaceInputSpec):
-    in_segm = File(
-        exists=True, mandatory=True, position=0, desc="Input discrete segmentation of the brain."
-    )
-    in_brainmask = File(exists=True, mandatory=True, position=1, desc="Brain mask.")
-    radius = traits.Int(default_value=2, usedefault=True, desc="Radius of dilation")
+class _BinaryDilationInputSpec(BaseInterfaceInputSpec):
+    in_mask = File(exists=True, mandatory=True, position=1, desc="input mask")
+    radius = traits.Int(2, usedefault=True, desc="Radius of dilation")
 
 
-class _CrownMaskOutputSpec(TraitedSpec):
+class _BinaryDilationOutputSpec(TraitedSpec):
     out_mask = File(exists=False, desc="Crown mask")
 
 
-class CrownMask(SimpleInterface):
+class BinaryDilation(SimpleInterface):
     """Dilate brain mask for computing the crown mask."""
 
-    input_spec = _CrownMaskInputSpec
-    output_spec = _CrownMaskOutputSpec
+    input_spec = _BinaryDilationInputSpec
+    output_spec = _BinaryDilationOutputSpec
 
     def _run_interface(self, runtime):
         import nibabel as nb
@@ -55,51 +52,36 @@ class CrownMask(SimpleInterface):
         from pathlib import Path
 
         # Open files
-        segm_img = nb.load(self.inputs.in_segm)
-        brainmask_img = nb.load(self.inputs.in_brainmask)
-
-        segm = np.bool_(segm_img.dataobj)
-        brainmask = np.bool_(brainmask_img.dataobj)
+        mask_img = nb.load(self.inputs.in_mask)
+        maskdata = np.bool_(mask_img.dataobj)
 
         # Obtain dilated brainmask
-        crown_mask, func_seg_mask = get_dilated_brainmask(
-            seg_mask=segm,
-            brainmask=brainmask,
+        dilated = image_binary_dilation(
+            maskdata,
             radius=self.inputs.radius,
         )
-        # Remove the brain from the crown mask
-        crown_mask[func_seg_mask] = False
-        crown_file = str((Path(runtime.cwd) / "crown_mask.nii.gz").absolute())
-        nii = nb.Nifti1Image(
-            crown_mask, brainmask_img.affine, brainmask_img.header
+        out_file = str((Path(runtime.cwd) / "dilated_mask.nii.gz").absolute())
+        out_img = mask_img.__class__(
+            dilated, mask_img.affine, mask_img.header
         )
-        nii.set_data_dtype("uint8")
-        nii.to_filename(crown_file)
-        self._results["out_mask"] = crown_file
-
+        out_img.set_data_dtype("uint8")
+        out_img.to_filename(out_file)
+        self._results["out_mask"] = out_file
         return runtime
 
 
-def get_dilated_brainmask(seg_mask, brainmask, radius=2):
-    """Obtain the brain mask dilated
+def image_binary_dilation(in_mask, radius=2):
+    """
+    Dilate the input binary mask.
+    
     Parameters
     ----------
-    atlaslabels: ndarray
-        A 3D binary array, resampled into ``img`` space.
-    brainmask: ndarray
-        A 3D binary array, resampled into ``img`` space.
-    radius: int, optional
+    in_mask: :obj:`numpy.ndarray`
+        A 3D binary array.
+    radius: :obj:`int`, optional
         The radius of the ball-shaped footprint for dilation of the mask.
     """
     from scipy import ndimage as ndi
     from skimage.morphology import ball
 
-    # Union of functionally and anatomically extracted masks
-    func_seg_mask = seg_mask | brainmask
-
-    if func_seg_mask.ndim != 3:
-        raise Exception("The brain mask should be a 3D array")
-
-    dilated_brainmask = ndi.binary_dilation(func_seg_mask, ball(radius))
-
-    return dilated_brainmask, func_seg_mask
+    return ndi.binary_dilation(in_mask.astype(bool), ball(radius)).astype(int)
