e42a7b41449c500fb25fc86ce2d2fa88e3ded281
mathiasg <mathiasg@stanford.edu>
2022-08-02 15:52:07 -0400

    ENH: Add interface for reorienting images

--- niworkflows/interfaces/nibabel.py
+++ niworkflows/interfaces/nibabel.py
@@ -21,6 +21,8 @@
 #     https://www.nipreps.org/community/licensing/
 #
 """Nibabel-based interfaces."""
+from pathlib import Path
+
 import numpy as np
 import nibabel as nb
 from nipype import logging
@@ -492,6 +494,69 @@ class MapLabels(SimpleInterface):
             newpath=runtime.cwd,
         )
         return runtime
+
+
+class ReorientImageInputSpec(BaseInterfaceInputSpec):
+    in_file = File(exists=True, mandatory=True, desc="Moving file")
+    target_file = File(
+        exists=True, xor=["target_orientation"], desc="Reference file to reorient to"
+    )
+    target_orientation = traits.Str(
+        xor=["target_file"], desc="Axis codes of coordinate system to reorient to"
+    )
+
+
+class ReorientImageOutputSpec(TraitedSpec):
+    out_file = File(desc="Reoriented file")
+
+
+class ReorientImage(SimpleInterface):
+    input_spec = ReorientImageInputSpec
+    output_spec = ReorientImageOutputSpec
+
+    def _run_interface(self, runtime):
+        self._results["out_file"] = reorient_image(
+            self.inputs.in_file,
+            target_file=self.inputs.target_file,
+            target_ornt=self.inputs.target_orientation,
+        )
+        return runtime
+
+
+def reorient_image(
+    in_file: str, *, target_file: str = None, target_ornt: str = None, newpath: str = None
+) -> str:
+    """
+    Reorient an image.
+
+    New orientation targets can be either another image, or a string representation of the
+    orientation axis.
+
+    Parameters
+    ----------
+    in_file : Image to be reoriented
+    target_file : Reference image of desired orientation
+    target_ornt : Orientation denoted by the first letter of each axis (i.e., "RAS", "LPI")
+    """
+    import nibabel as nb
+
+    img = nb.load(in_file)
+    img_axcodes = nb.aff2axcodes(img.affine)
+    in_ornt = nb.orientations.axcodes2ornt(img_axcodes)
+
+    if target_file:
+        target_img = nb.load(target_file)
+        target_ornt = nb.aff2axcodes(target_img.affine)
+
+    out_ornt = nb.orientations.axcodes2ornt(target_ornt)
+    ornt_xfm = nb.orientations.ornt_transform(in_ornt, out_ornt)
+    reoriented = img.as_reoriented(ornt_xfm)
+
+    if newpath is None:
+        newpath = Path()
+    out_file = str((Path(newpath) / "reoriented.nii.gz").absolute())
+    reoriented.to_filename(out_file)
+    return out_file
 
 
 def _gen_reference(
--- niworkflows/interfaces/tests/test_nibabel.py
+++ niworkflows/interfaces/tests/test_nibabel.py
@@ -29,7 +29,9 @@ import numpy as np
 import nibabel as nb
 import pytest
 
-from ..nibabel import Binarize, ApplyMask, SplitSeries, MergeSeries, MergeROIs, MapLabels
+from ..nibabel import (
+    Binarize, ApplyMask, SplitSeries, MergeSeries, MergeROIs, MapLabels, ReorientImage
+)
 
 
 @pytest.fixture
@@ -280,3 +282,55 @@ def test_map_labels(tmpdir, data, mapping, tojson, expected):
     Path(in_file).unlink()
     if tojson:
         Path(map_file).unlink()
+
+
+def create_save_img(ornt: str):
+    data = np.random.rand(2, 2, 2)
+    img = nb.Nifti1Image(data, affine=np.eye(4))
+    # img will alway be in RAS at the start
+    ras = nb.orientations.axcodes2ornt("RAS")
+    if ornt != 'RAS':
+        new = nb.orientations.axcodes2ornt(ornt)
+        xfm = nb.orientations.ornt_transform(ras, new)
+        img = img.as_reoriented(xfm)
+    out_file = f'{uuid.uuid4()}.nii.gz'
+    img.to_filename(out_file)
+    return out_file
+
+
+@pytest.mark.parametrize(
+    "in_ornt,out_ornt",
+    [
+        ("RAS", "RAS"),
+        ("RAS", "LAS"),
+        ("LAS", "RAS"),
+        ("RAS", "RPI"),
+        ("LPI", "RAS"),
+    ],
+)
+def test_reorient_image(tmpdir, in_ornt, out_ornt):
+    tmpdir.chdir()
+
+    in_file = create_save_img(ornt=in_ornt)
+    in_img = nb.load(in_file)
+    assert ''.join(nb.aff2axcodes(in_img.affine)) == in_ornt
+
+    # test string representation
+    res = ReorientImage(in_file=in_file, target_orientation=out_ornt).run()
+    out_file = res.outputs.out_file
+    out_img = nb.load(out_file)
+    assert ''.join(nb.aff2axcodes(out_img.affine)) == out_ornt
+    Path(out_file).unlink()
+
+    # test with target file
+    target_file = create_save_img(ornt=out_ornt)
+    target_img = nb.load(target_file)
+    assert ''.join(nb.aff2axcodes(target_img.affine)) == out_ornt
+    res = ReorientImage(in_file=in_file, target_file=target_file).run()
+    out_file = res.outputs.out_file
+    out_img = nb.load(out_file)
+    assert ''.join(nb.aff2axcodes(out_img.affine)) == out_ornt
+
+    # cleanup
+    for f in (in_file, target_file, out_file):
+        Path(f).unlink()
