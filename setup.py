8b3b9581a2695952e0768f7d807fc23064e9d57d
Chris Markiewicz <effigies@gmail.com>
2023-09-17 08:10:15 -0400

    MNT: Drop deprecated alias in declarative config

--- setup.py
+++ setup.py
@@ -6,14 +6,10 @@ from setuptools import setup
 import versioneer
 
 # Use setup_requires to let setuptools complain if it's too old for a feature we need
-# 30.3.0 allows us to put most metadata in setup.cfg
-# 30.4.0 gives us options.packages.find
 # 40.8.0 includes license_file, reducing MANIFEST.in requirements
 #
-# To install, 30.4.0 is enough, but if we're building an sdist, require 40.8.0
-# This imposes a stricter rule on the maintainer than the user
 # Keep the installation version synchronized with pyproject.toml
-SETUP_REQUIRES = [f"setuptools >= {'40.8.0' if 'sdist' in sys.argv else '30.4.0'}"]
+SETUP_REQUIRES = ["setuptools >= 40.8.0"]
 
 # This enables setuptools to install wheel on-the-fly
 if "bdist_wheel" in sys.argv:
