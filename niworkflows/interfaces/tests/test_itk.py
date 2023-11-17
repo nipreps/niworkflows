bfee73b437bc61e5d0e425a6b9b036a0ed59a993
Chris Markiewicz <markiewicz@stanford.edu>
2023-11-17 15:06:49 -0500

    RF: Replace Convert3d with nitransforms in MCFLIRT2ITK (#835)
    * TEST: Add regression test for MCFLIRT2ITK
    
    * TEST: Update test to account for minor differences with nitransforms
    
    * RF: Replace Convert3d with nitransforms in MCFLIRT2ITK
    
    * MNT: Remove convert3d from the Dockerfile

--- niworkflows/interfaces/tests/test_itk.py
+++ niworkflows/interfaces/tests/test_itk.py
@@ -20,10 +20,17 @@
 #
 #     https://www.nipreps.org/community/licensing/
 #
+import re
+from pathlib import Path
+
+import numpy as np
 import pytest
-from ..itk import _applytfms
-from ... import data
 from nipype.interfaces.ants.base import Info
+from nipype.pipeline import engine as pe
+
+from ... import data
+from ..itk import MCFLIRT2ITK, _applytfms
+from .data import load_test_data
 
 
 @pytest.mark.skipif(Info.version() is None, reason="Missing ANTs")
@@ -31,8 +38,8 @@ from nipype.interfaces.ants.base import Info
 @pytest.mark.parametrize("copy_dtype", (True, False))
 @pytest.mark.parametrize("in_dtype", ("i2", "f4"))
 def test_applytfms(tmpdir, ext, copy_dtype, in_dtype):
-    import numpy as np
     import nibabel as nb
+    import numpy as np
 
     in_file = str(tmpdir / ("src" + ext))
     nii = nb.Nifti1Image(np.zeros((5, 5, 5), dtype=np.float32), np.eye(4))
@@ -52,3 +59,81 @@ def test_applytfms(tmpdir, ext, copy_dtype, in_dtype):
     assert np.allclose(nii.get_fdata(), out_nii.get_fdata())
     if copy_dtype:
         assert nii.get_data_dtype() == out_nii.get_data_dtype()
+
+
+def test_MCFLIRT2ITK(tmp_path):
+    # Test that MCFLIRT2ITK produces output that is consistent with convert3d
+    test_data = load_test_data()
+
+    fsl2itk = pe.Node(
+        MCFLIRT2ITK(
+            in_files=[str(test_data / "MAT_0098"), str(test_data / "MAT_0099")],
+            in_reference=str(test_data / "boldref.nii"),
+            in_source=str(test_data / "boldref.nii"),
+        ),
+        name="fsl2itk",
+        base_dir=str(tmp_path),
+    )
+
+    res = fsl2itk.run()
+    out_file = Path(res.outputs.out_file)
+
+    assert out_file.exists()
+    lines = out_file.read_text().splitlines()
+
+    assert lines[:2] == [
+        "#Insight Transform File V1.0",
+        "#Transform 0",
+    ]
+    assert re.match(
+        r"Transform: (MatrixOffsetTransformBase|AffineTransform)_(float|double)_3_3",
+        lines[2],
+    )
+    assert lines[3].startswith("Parameters: ")
+    assert lines[4] == "FixedParameters: 0 0 0"
+    offset = 1 if lines[5] == "" else 0
+    assert lines[5 + offset] == "#Transform 1"
+    assert lines[6 + offset] == lines[2]
+    assert lines[7 + offset].startswith("Parameters: ")
+
+    params0 = np.array([float(p) for p in lines[3].split(" ")[1:]])
+    params1 = np.array([float(p) for p in lines[7 + offset].split(" ")[1:]])
+    # Empirically determined
+    assert np.allclose(
+        params0,
+        np.array(
+            [
+                9.99998489e-01,
+                -4.36657508e-04,
+                -1.52316526e-03,
+                4.36017740e-04,
+                9.99999777e-01,
+                -2.10558666e-04,
+                1.52334852e-03,
+                2.09440681e-04,
+                9.99998624e-01,
+                -1.28961869e-03,
+                6.93155516e-02,
+                -1.12375673e-02,
+            ]
+        ),
+    )
+    assert np.allclose(
+        params1,
+        np.array(
+            [
+                9.99999130e-01,
+                -4.60021530e-04,
+                -1.28828576e-03,
+                4.58910652e-04,
+                9.99999648e-01,
+                -3.90485877e-04,
+                1.28764980e-03,
+                3.89513646e-04,
+                9.99999178e-01,
+                -9.19541650e-03,
+                7.45419094e-02,
+                -8.95843238e-03,
+            ]
+        ),
+    )
