343cdb3e98e2a5dfa127c55b1354ae7c9eaddae4
Mathias Goncalves <goncalves.mathias@gmail.com>
2025-01-09 14:01:07 -0500

    ENH: Parse kwargs to allow no T1w
    Also, adds a default to the expected kwarg `anat_only` to allow bare initialization

--- niworkflows/interfaces/bids.py
+++ niworkflows/interfaces/bids.py
@@ -253,11 +253,18 @@ class BIDSDataGrabber(SimpleInterface):
     >>> bids_src.inputs.subject_data = bids_collect_data(
     ...     str(datadir / 'ds114'), '01', bids_validate=False)[0]
     >>> bids_src.inputs.subject_id = '01'
+    >>> bids_src._require_t1w
+    True
+    >>> bids_src._require_funcs
+    True
     >>> res = bids_src.run()
     >>> res.outputs.t1w  # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
     ['.../ds114/sub-01/ses-retest/anat/sub-01_ses-retest_T1w.nii.gz',
      '.../ds114/sub-01/ses-test/anat/sub-01_ses-test_T1w.nii.gz']
 
+    >>> bids_src = BIDSDataGrabber(require_t1w=False)
+    >>> bids_src._require_t1w
+    False
     """
 
     input_spec = _BIDSDataGrabberInputSpec
@@ -265,12 +272,13 @@ class BIDSDataGrabber(SimpleInterface):
     _require_funcs = True
 
     def __init__(self, *args, **kwargs):
-        anat_only = kwargs.pop('anat_only')
+        anat_only = kwargs.pop('anat_only', None)
         anat_derivatives = kwargs.pop('anat_derivatives', None)
+        require_t1w = kwargs.pop('require_t1w', True)
         super().__init__(*args, **kwargs)
         if anat_only is not None:
             self._require_funcs = not anat_only
-        self._require_t1w = anat_derivatives is None
+        self._require_t1w = require_t1w and anat_derivatives is None
 
     def _run_interface(self, runtime):
         bids_dict = self.inputs.subject_data
